41aa00d7a6a7dcc73b5a6b323c21f97a7df21441
Luke Cartledge <luke@lukecartledge.com>
2016-07-19 17:05:42 +0100

    18 - Entering players

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,9 @@
 RSpec.feature "Name Entry Form", :type => :feature do
   scenario "Players enter their names" do
-    visit "/names"
+    visit "/"
 
-    fill_in "Player 1", :with => "Gringo"
-    fill_in "Player 2", :with => "Amigo"
+    fill_in :player_1, with: 'Gringo'
+    fill_in :player_2, with:  'Amigo'
     click_button "Enter Names"
 
     expect(page).to have_text("Player 1 is called Gringo and Player 2 is called Amigo")
