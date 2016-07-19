8658ea751f2d8acd3d6b38cb7874b7b159f178e7
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-19 17:07:19 +0100

    local changes

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,8 @@
-RSpec.feature "Name Entry Form", :type => :feature do
+feature "Name Entry Form" do
   scenario "Players enter their names" do
-    visit "/names"
-
-    fill_in "Player 1", :with => "Gringo"
-    fill_in "Player 2", :with => "Amigo"
+    visit ("/")
+    fill_in :player_1 , with: "Gringo"
+    fill_in :player_2 , with: "Amigo"
     click_button "Enter Names"
 
     expect(page).to have_text("Player 1 is called Gringo and Player 2 is called Amigo")
